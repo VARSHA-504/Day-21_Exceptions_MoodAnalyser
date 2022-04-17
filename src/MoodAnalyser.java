a3780754f252bd16c243f8aecbed458332fd1ec0
varsha-504 <antargangivarsha.com>
2022-04-17 23:15:02 +0530

    first commit

---
+++ src/MoodAnalyser.java
@@ -0,0 +1,17 @@
+public class MoodAnalyser {
+    private String msg;
+
+    public MoodAnalyser() {
+    }
+
+    public MoodAnalyser(String msg){
+        this.msg = msg;
+    }
+
+    public String analyseMood() {
+        if(msg.contains("sad")){
+            return "Sad";
+        }
+        return "Happy";
+    }
+}
